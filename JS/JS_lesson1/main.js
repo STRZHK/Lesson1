26e2220b61d404b90a9bd050a0428caa4f37355d
STRZHK <oleksa.stryzhak@gmail.com>
2022-04-14 02:36:59 +0300

    HW_lesson1_JS_Stryzhak_Oleksandr

---
+++ JS/JS_lesson1/main.js
@@ -0,0 +1,69 @@
+// let a; a = 'hello'; alert(a); console.log(a)
+// let b; b = 'owu'; alert(b); console.log(b)
+// let ua = 'ua'; alert(ua); console.log(ua)
+// let q = '1'; alert(q); console.log(q)
+// let w = '10'; alert(w); console.log(w)
+// let e = '-999'; alert(e); console.log(e)
+// let r = '123'; alert(r); console.log(r)
+// let t = '3.14'; alert(t); console.log(t)
+// let y = '2.7'; alert(y); console.log(y)
+// let d = '16'; alert(d); console.log(d)
+// let fl = 'true'; alert(fl); console.log(fl)
+// let f = 'false'; alert(f); console.log(f)
+//
+// document.write(a,b,ua,q,w,e,r,t,y,d,fl,f);
+//
+//
+// let book;
+// book = {
+//     name: 'some book',
+//     pages: 300,
+//     genre: 'some gener'
+// };
+//
+//
+// let book;
+// book = {
+//     name: 'some book',
+//     pages: 300,
+//     genre: 'some gener',
+//     authors: [
+//         {name: 'author1', surname:'author1sur'},
+//         {name: 'author2', surname:'author2sur'},
+//     ]
+// };
+//
+//
+// let firstName = 'Stryzhak';
+// let middleName = 'Serhiovych';
+// let lastName = 'Oleksandr';
+// let person = {firstName, middleName, lastName};
+// console.log(person)
+//
+// let name = 'Oleksandr';
+// let middlename = 'Serhiovych';
+// let age = 23;
+// let person = {name, middlename, age};
+// let field = prompt('enter field');
+// console.log(person[field])
+//
+//
+// let a = 100; let b = '100'; let c = true;
+// console.log(typeof a); /*number*/
+// console.log(typeof b); /*string*/
+// console.log(typeof c); /*boolean*/
+//
+// let books = [
+//     {name: 'some book', pages: 300, genre: 'some genre', authors: [
+//         {firstname: 'author1', surname:'author1sur'},
+//         {firstname: 'author2', surname:'author2sur'}, ]},
+//     {name: 'some book 1', pages: 400, genre: 'some genre 1', authors: [
+//             {firstname: 'author3', surname:'author3sur'},
+//             {firstname: 'author4', surname:'author4sur'}, ]},
+//     {name: 'some book 2', pages: 500, genre: 'some genre 2', authors: [
+//             {firstname: 'author5', surname:'author5sur'},
+//             {firstname: 'author6', surname:'author6sur'}, ]},
+//     {name: 'some book 3', pages: 601, genre: 'some genre 3', authors: [
+//             {firstname: 'author7', surname:'author4sur'},
+//             {firstname: 'author8', surname:'author8sur'}, ]}
+// ]
